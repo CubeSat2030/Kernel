f9a2aaef34ad0662b01a007b022e7b3dd7250674
Nathan-Busse <bussenathan@icloud.com>
2026-06-05 18:28:32 +0200

    Add boot fixup data, kernel image, and initial test_init program
    - Create `fixup4.dat` to provide necessary boot configurations.
    - Add `kernel7.img` for the system's kernel image.
    - Introduce `test_init.c` for preliminary system initialization and diagnostics.
      - Mounts essential filesystems: proc, sysfs, and devtmpfs.
      - Prints kernel version and lists devices in /dev.
      - Informs the user of the lack of a real root mount before causing kernel panic.

--- Core/OS_DevTools/KabotSat_ImageGen/KabotSat_ImageGen.py
+++ Core/OS_DevTools/KabotSat_ImageGen/KabotSat_ImageGen.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4a8e8ae3a96f22da40612190d7e3ca764d12bd222417472384d375342b4723d6
-size 47473
+oid sha256:c33833d0ff110cafb23ad15f3ff0037b5b618920e4998a908c33c010486addfc
+size 43219
