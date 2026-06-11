8ca07063adbe7e5bb2cf720fdda5f1752d39ba8c
Nathan-Busse <bussenathan@icloud.com>
2026-06-11 16:52:37 +0200

    CubeSat is Alive!

--- Core/src/include/generated/utsversion.h
+++ Core/src/include/generated/utsversion.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:80c2d05aef5602a6bd96e19189e06cb3260aa6ef1b8e12d723614bdd2fcf19ad
+oid sha256:d83c0af5dbc4a738814e027b6b3475ca523ed9fc01309b48f399916d5087fd57
 size 55
