5e65d52f4c2f753e3c4325273a0db99fb69b7b2d
Nathan-Busse <bussenathan@icloud.com>
2026-06-11 13:19:06 +0200

    The most stable build of the Kernel as of 11 June 2026 @ 13:20 PM SAST.

--- Core/src/include/generated/utsversion.h
+++ Core/src/include/generated/utsversion.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:28d54648a52c5680f9b43de3a8154546b4e2f24f3055965de89390441ac8b468
+oid sha256:80c2d05aef5602a6bd96e19189e06cb3260aa6ef1b8e12d723614bdd2fcf19ad
 size 55
